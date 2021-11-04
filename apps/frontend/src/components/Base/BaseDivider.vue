ae0f3044678953b2b1ac93dfa62499bc0ef1d996
talhadogrul <talha.dogurl@outlook.com>
2021-11-04 18:54:26 +0300

    feat(frontend): divider component added

---
+++ apps/frontend/src/components/Base/BaseDivider.vue
@@ -0,0 +1,21 @@
+<template>
+  <hr class="divider" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup () {
+    
+
+    return {}
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.divider {
+  
+}
+</style>
