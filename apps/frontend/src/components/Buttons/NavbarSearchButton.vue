cecac8d96f12901271e1633e0aa45d87260585e8
talhadogrul <talha.dogurl@outlook.com>
2021-11-09 19:05:10 +0300

    feat(frontend): navbar search button added

---
+++ apps/frontend/src/components/Buttons/NavbarSearchButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <BaseButton isText size="none" bgColor="transparent" textColor="white">
+      <SearchIcon class="inline mr-6" />
+      Anywhere on the keyboard
+  </BaseButton>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import BaseButton from '../Base/BaseButton.vue'
+import SearchIcon from '../Icons/SearchIcon.vue'
+
+export default defineComponent({
+  components: { SearchIcon, BaseButton },
+  setup () {
+    
+
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
